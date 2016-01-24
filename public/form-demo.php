3098be7a0bf53bcefa94e8ef1e6b704c85f7cc9f
dong@tjuacm <zhengdongjian@tju.edu.cn>
2016-01-24 19:13:36 +0800

    a form demo for user registeration

--- public/form-demo.php
+++ public/form-demo.php
@@ -1,15 +1,140 @@
 <html>
     <head>
-        <title>测试!</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="author" content="ZhengDongjian"/>
+        <title>测试!</title>
+        
+        <!-- 新 Bootstrap 核心 CSS 文件 -->
+        <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+        <script src="http://cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+        <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+        <script src="http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <style>
+            .form-signup {
+                max-width: 500px;
+                padding-top: 50px;
+                padding-right: 15px;
+                padding-bottom: 15px;
+                padding-left: 15px;
+                margin-top: auto;
+                margin-right: auto;
+                margin-bottom: auto;
+                margin-left: 10px;
+            }
+        </style>
     </head>
+
     <body>
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
-            用户名: <input type="text" name="username" /><br/>
-            密码: <input type="text" name="password" /><br/>
-            确认密码: <input type="text" name="password2" /><br/>
-            邮箱: <input type="text" name="email" /><br/>
-            <input type="submit" name="submit" />
+        <form class="form-signup" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <label class="control-label sr-only">用户名: </label>
+                    <input class="form-control field" type="text" name="username" placeholder="用户名: " required autofocus />
+                </div>
+                <div class="form-group">
+                    <label class="control-label sr-only">密码: </label>
+                    <input class="form-control field" type="text" name="password" placeholder="密码: " required autofocus />
+                </div>
+                <div class="form-group">
+                    <label class="control-label sr-only">确认密码: </label>
+                    <input class="form-control field" type="text" name="password2" placeholder="确认密码: " required autofocus />
+                </div>
+                <div class="form-group">
+                    <label class="control-label sr-only">邮箱: </label>
+                    <input class="form-control field" type="text" name="email" placeholder="邮箱: " required autofocus />
+                </div>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">注册</button>
+            </div>
         </form>
     </body>
 </html>
+
+<?php
+/* **********************************************
+
+  File Name: registrationHandler.php
+
+  Author: zhengdongjian@tju.edu.cn
+
+  Created Time: Sun 24 Jan 2016 06:17:45 PM CST
+
+*********************************************** */
+
+
+/*
+ * 检查输入并返回结果
+ */
+
+/*
+ * 错误常量
+ */
+define("OK", 0);
+define("USERNAME_INVALID", 100);
+define("PASSWORD_INVALID", 200);
+define("PASSWORD_DIFF", 201);
+define("EMAIL_INVALID", 300);
+$messageBox = array(
+    "OK" => "OK",
+    "USERNAME_INVALID" => "用户名不合法！用户名只能包含大小写字母和下划线，且为6-32位长！",
+    "PASSWORD_INVALID" => "密码不合法！密码只能包含大小写字母、下划线和数字，且为8-32位长！",
+    "PASSWORD_DIFF" => "两次输入密码不相同！",
+    "EMAIL_INVALID" => "邮箱地址不合法！"
+);
+
+$username = $password = $password2 = $email = "";
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $username = inputFilter($_POST["username"]);
+    $password = inputFilter($_POST["passowrd"]);
+    $password2 = inputFilter($_POST["password2"]);
+    $email = inputFilter($_POST["email"]);
+
+    $errorCode = inputChecker($username, $password, $password2, $email);
+    if ($errorCode !== 0) {
+        alertError($errorCode);
+    } else {
+    }
+}
+
+/*
+ * 过滤输入
+ */
+function inputFilter($sourceData) {
+    return htmlspecialchars(stripslashes(trim($sourceData)));
+}
+
+/*
+ * 检查输入错误，如果有错误返回出错信息
+ */
+function inputChecker($username, $password, $password2, $email) {
+    if (!preg_match("/^[\w]{6,32}$/", $username)) {
+        return USERNAME_INVALID; //用户名，大小写字母和下划线, 长度6-32字符
+    } else if (!preg_match("/^[\w\d]{8,32}$/", $password)) {
+        return PASSWORD_INVALID; //密码, 大小写下划线和数字, 长度8-32位, 数据库中为50位长.
+    } else if ($password !== $password2) {
+        return PASSWORD_DIFF; //两次密码不同
+    } else if (!preg_match("/([\w\-]+\@[\w\-]+)(\.[\w\-]+)+/", $email)) {
+        return EMAIL_INVALID; //邮箱不合法
+    } else {
+        return OK;
+    }
+}
+
+/* 
+ * 返回错误提示
+ */
+function alertError($errorCode) {
+    $errorMessage = $messageBox[$errorCode];
+    echo "left:";
+    foreach ($messageBox as $key => $value) {
+        echo "a<br/>";
+        echo $key." => ".$value."<br/>";
+    }
+    echo $messageBox["$errorCode"];
+    echo ":right";
+    echo "<script> alert($errorMessage + $errorCode); </script>";
+}
+
+?>
