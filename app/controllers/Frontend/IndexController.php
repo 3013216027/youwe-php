2560fde86c31d7d1d02c1129ca0b84e6f8f3d3b6
zhengdongjian@asus|ubuntu 15.10 <zhengdongjian@tju.edu.cn>
2016-02-20 14:40:44 +0800

    test

--- app/controllers/Frontend/IndexController.php
+++ app/controllers/Frontend/IndexController.php
@@ -1,10 +1,12 @@
-<?php namespace Frontend;
+<?php
+namespace Frontend;
 use \Redirect;
 
 class IndexController extends BaseController {
 
     public function index() {
-        return Redirect::to('http://www.tju.edu.cn');
+        //return Redirect::to('http://www.tju.edu.cn');
+        echo 'publish success! &ltinfo from:/app/controllers/Frontend/IndexController.php&gt';
     }
 
 }
